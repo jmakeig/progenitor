a58c636436ca93df3eef26e3176e117cf211104f
Justin Makeig <jmpublic+github@makeig.com>
2018-08-20 15:57:20 -0700

    Parameterizes number of UUIDs generated

--- script/uuid.js
+++ script/uuid.js
@@ -1,4 +1,11 @@
 #! /usr/bin/env node
 
 const uuid = require('uuid');
-process.stdout.write(uuid() + '\n');
+const MAX = 500;
+const count = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
+for (let i = 0; i < Math.min(count, MAX); i++) {
+  process.stdout.write(uuid() + '\n');
+}
+if (count > MAX) {
+  process.stderr.write(`Truncated from ${count} to ${MAX}\n`);
+}
