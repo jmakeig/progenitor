4af22dd3b23838b614391091cad0c5a075b0608a
Justin Makeig <jmpublic+github@makeig.com>
2018-08-28 14:07:04 -0700

    Extracts out dom-helper

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,29 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import replace from 'rollup-plugin-replace';
-import babel from 'rollup-plugin-babel';
+// import babel from 'rollup-plugin-babel';
 
 export default {
   //input: 'browser/src/main.js',
   input: 'experimentation/hier.js',
   output: {
-    file: 'browser/bundle.js',
+    file: 'experimentation/bundle.js',
     format: 'iife',
-    sourcemap: true,
+    sourcemap: true
   },
   plugins: [
     replace({
-      'process.env.NODE_ENV': JSON.stringify('development'),
+      'process.env.NODE_ENV': JSON.stringify('development')
     }),
     nodeResolve({
       jsnext: true,
-      main: true,
+      main: true
     }),
     commonjs({
-      include: 'node_modules/**',
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-  ],
+      include: 'node_modules/**'
+    })
+    // babel({
+    //   exclude: 'node_modules/**',
+    // }),
+  ]
 };
