1c82b46cec964c9086b3cf48379fae5f63fd0fb3
Justin Makeig <jmpublic+github@makeig.com>
2018-08-28 09:34:37 -0700

    Useage notes and better error handling

--- script/uuid.js
+++ script/uuid.js
@@ -1,8 +1,20 @@
 #! /usr/bin/env node
 
+/*
+ * Usage: 
+ *   ./uuid.js
+ *      Generate a sinle UUID
+ *   ./uuid.js N
+ *      Generate N UUIDs, where N is parsed as a number
+ */
+
 const uuid = require('uuid');
 const MAX = 500;
 const count = process.argv[2] ? parseInt(process.argv[2], 10) : 1;
+if (Number.isNaN(count)) {
+  process.stderr.write(`Not a number\n`);
+  process.exit(1);
+}
 for (let i = 0; i < Math.min(count, MAX); i++) {
   process.stdout.write(uuid() + '\n');
 }
