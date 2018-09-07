a1fc060e5eb9ebd0c2e0e381b857650f4f2dbae3
Justin Makeig <jmpublic+github@makeig.com>
2018-09-07 09:12:21 -0700

    Formatting

--- experimentation/react-helper.js
+++ experimentation/react-helper.js
@@ -76,51 +76,51 @@ export function renderInto(component, element) {
   return ReactDOM.render(component, element);
 }
 
-export const toFragment = (...p) => el(React.Fragment, ...p);
-export const empty      = ()     => toFragment();
+/* prettier-ignore */ export const toFragment = (...p) => el(React.Fragment, ...p);
+/* prettier-ignore */ export const empty      = ()     => toFragment();
 
-export const header     = (...p) => el('header', ...p);
-export const nav        = (...p) => el('nav', ...p);
-export const footer     = (...p) => el('footer', ...p);
-export const div        = (...p) => el('div', ...p);
-export const p          = (...p) => el('p', ...p);
-export const h1         = (...p) => el('h1', ...p);
-export const h2         = (...p) => el('h2', ...p);
-export const h3         = (...p) => el('h3', ...p);
-export const h4         = (...p) => el('h4', ...p);
-export const h5         = (...p) => el('h5', ...p);
-export const h6         = (...p) => el('h6', ...p);
+/* prettier-ignore */ export const header     = (...p) => el('header', ...p);
+/* prettier-ignore */ export const nav        = (...p) => el('nav', ...p);
+/* prettier-ignore */ export const footer     = (...p) => el('footer', ...p);
+/* prettier-ignore */ export const div        = (...p) => el('div', ...p);
+/* prettier-ignore */ export const p          = (...p) => el('p', ...p);
+/* prettier-ignore */ export const h1         = (...p) => el('h1', ...p);
+/* prettier-ignore */ export const h2         = (...p) => el('h2', ...p);
+/* prettier-ignore */ export const h3         = (...p) => el('h3', ...p);
+/* prettier-ignore */ export const h4         = (...p) => el('h4', ...p);
+/* prettier-ignore */ export const h5         = (...p) => el('h5', ...p);
+/* prettier-ignore */ export const h6         = (...p) => el('h6', ...p);
 
-export const ul         = (...p) => el('ul', ...p);
-export const ol         = (...p) => el('ol', ...p);
-export const li         = (...p) => el('li', ...p);
-export const dl         = (...p) => el('dl', ...p);
-export const dt         = (...p) => el('dt', ...p);
-export const dd         = (...p) => el('dd', ...p);
+/* prettier-ignore */ export const ul         = (...p) => el('ul', ...p);
+/* prettier-ignore */ export const ol         = (...p) => el('ol', ...p);
+/* prettier-ignore */ export const li         = (...p) => el('li', ...p);
+/* prettier-ignore */ export const dl         = (...p) => el('dl', ...p);
+/* prettier-ignore */ export const dt         = (...p) => el('dt', ...p);
+/* prettier-ignore */ export const dd         = (...p) => el('dd', ...p);
 
-export const table      = (...p) => el('table', ...p);
-export const thead      = (...p) => el('thead', ...p);
-export const tfoot      = (...p) => el('tfoot', ...p);
-export const tbody      = (...p) => el('tbody', ...p);
-export const tr         = (...p) => el('tr', ...p);
-export const th         = (...p) => el('th', ...p);
-export const td         = (...p) => el('td', ...p);
+/* prettier-ignore */ export const table      = (...p) => el('table', ...p);
+/* prettier-ignore */ export const thead      = (...p) => el('thead', ...p);
+/* prettier-ignore */ export const tfoot      = (...p) => el('tfoot', ...p);
+/* prettier-ignore */ export const tbody      = (...p) => el('tbody', ...p);
+/* prettier-ignore */ export const tr         = (...p) => el('tr', ...p);
+/* prettier-ignore */ export const th         = (...p) => el('th', ...p);
+/* prettier-ignore */ export const td         = (...p) => el('td', ...p);
 
-export const span       = (...p) => el('span', ...p);
-export const a          = (...p) => el('a', ...p);
-export const em         = (...p) => el('em', ...p);
-export const strong     = (...p) => el('strong', ...p);
-export const mark       = (...p) => el('mark', ...p);
+/* prettier-ignore */ export const span       = (...p) => el('span', ...p);
+/* prettier-ignore */ export const a          = (...p) => el('a', ...p);
+/* prettier-ignore */ export const em         = (...p) => el('em', ...p);
+/* prettier-ignore */ export const strong     = (...p) => el('strong', ...p);
+/* prettier-ignore */ export const mark       = (...p) => el('mark', ...p);
 
-export const input      = (...p) => el('input', { type: 'text' }, ...p);
-export const button     = (...p) => el('button', ...p);
-export const text       = input;
-export const textarea   = (...p) => el('textarea', ...p);
-export const checkbox   = (...p) => el('input', { type: 'checkbox' }, ...p);
-export const radio      = (...p) => el('input', { type: 'radio' }, ...p);
-export const select     = (...p) => el('select', ...p);
-export const option     = (...p) => el('option', ...p);
-export const file       = (...p) => el('input', { type: 'file' }, ...p);
+/* prettier-ignore */ export const input      = (...p) => el('input', { type: 'text' }, ...p);
+/* prettier-ignore */ export const button     = (...p) => el('button', ...p);
+/* prettier-ignore */ export const text       = input;
+/* prettier-ignore */ export const textarea   = (...p) => el('textarea', ...p);
+/* prettier-ignore */ export const checkbox   = (...p) => el('input', { type: 'checkbox' }, ...p);
+/* prettier-ignore */ export const radio      = (...p) => el('input', { type: 'radio' }, ...p);
+/* prettier-ignore */ export const select     = (...p) => el('select', ...p);
+/* prettier-ignore */ export const option     = (...p) => el('option', ...p);
+/* prettier-ignore */ export const file       = (...p) => el('input', { type: 'file' }, ...p);
 
-export const br         = (...p) => el('br', ...p);
-export const hr         = (...p) => el('hr', ...p);
+/* prettier-ignore */ export const br         = (...p) => el('br', ...p);
+/* prettier-ignore */ export const hr         = (...p) => el('hr', ...p);
