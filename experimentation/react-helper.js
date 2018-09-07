cb40433fec431544128ea1482130766c10e33785
Justin Makeig <jmpublic+github@makeig.com>
2018-09-07 09:00:35 -0700

    Formatting

--- experimentation/react-helper.js
+++ experimentation/react-helper.js
@@ -39,7 +39,7 @@ function isReact(obj) {
   );
 }
 
-function reactComponent(name, ...stuff) {
+function el(name, ...stuff) {
   const props = {};
   const children = [];
 
@@ -76,55 +76,51 @@ export function renderInto(component, element) {
   return ReactDOM.render(component, element);
 }
 
-export const toFragment = (...rest) => reactComponent(React.Fragment, ...rest);
-export const empty = () => toFragment();
+export const toFragment = (...p) => el(React.Fragment, ...p);
+export const empty      = ()     => toFragment();
 
-export const header = (...rest) => reactComponent('header', ...rest);
-export const nav = (...rest) => reactComponent('nav', ...rest);
-export const footer = (...rest) => reactComponent('footer', ...rest);
-export const div = (...rest) => reactComponent('div', ...rest);
-export const p = (...rest) => reactComponent('p', ...rest);
-export const h1 = (...rest) => reactComponent('h1', ...rest);
-export const h2 = (...rest) => reactComponent('h2', ...rest);
-export const h3 = (...rest) => reactComponent('h3', ...rest);
-export const h4 = (...rest) => reactComponent('h4', ...rest);
-export const h5 = (...rest) => reactComponent('h5', ...rest);
-export const h6 = (...rest) => reactComponent('h6', ...rest);
+export const header     = (...p) => el('header', ...p);
+export const nav        = (...p) => el('nav', ...p);
+export const footer     = (...p) => el('footer', ...p);
+export const div        = (...p) => el('div', ...p);
+export const p          = (...p) => el('p', ...p);
+export const h1         = (...p) => el('h1', ...p);
+export const h2         = (...p) => el('h2', ...p);
+export const h3         = (...p) => el('h3', ...p);
+export const h4         = (...p) => el('h4', ...p);
+export const h5         = (...p) => el('h5', ...p);
+export const h6         = (...p) => el('h6', ...p);
 
-export const ul = (...rest) => reactComponent('ul', ...rest);
-export const ol = (...rest) => reactComponent('ol', ...rest);
-export const li = (...rest) => reactComponent('li', ...rest);
-export const dl = (...rest) => reactComponent('dl', ...rest);
-export const dt = (...rest) => reactComponent('dt', ...rest);
-export const dd = (...rest) => reactComponent('dd', ...rest);
+export const ul         = (...p) => el('ul', ...p);
+export const ol         = (...p) => el('ol', ...p);
+export const li         = (...p) => el('li', ...p);
+export const dl         = (...p) => el('dl', ...p);
+export const dt         = (...p) => el('dt', ...p);
+export const dd         = (...p) => el('dd', ...p);
 
-export const table = (...rest) => reactComponent('table', ...rest);
-export const thead = (...rest) => reactComponent('thead', ...rest);
-export const tfoot = (...rest) => reactComponent('tfoot', ...rest);
-export const tbody = (...rest) => reactComponent('tbody', ...rest);
-export const tr = (...rest) => reactComponent('tr', ...rest);
-export const th = (...rest) => reactComponent('th', ...rest);
-export const td = (...rest) => reactComponent('td', ...rest);
+export const table      = (...p) => el('table', ...p);
+export const thead      = (...p) => el('thead', ...p);
+export const tfoot      = (...p) => el('tfoot', ...p);
+export const tbody      = (...p) => el('tbody', ...p);
+export const tr         = (...p) => el('tr', ...p);
+export const th         = (...p) => el('th', ...p);
+export const td         = (...p) => el('td', ...p);
 
-export const span = (...rest) => reactComponent('span', ...rest);
-export const a = (...rest) => reactComponent('a', ...rest);
-export const em = (...rest) => reactComponent('em', ...rest);
-export const strong = (...rest) => reactComponent('strong', ...rest);
-export const mark = (...rest) => reactComponent('mark', ...rest);
+export const span       = (...p) => el('span', ...p);
+export const a          = (...p) => el('a', ...p);
+export const em         = (...p) => el('em', ...p);
+export const strong     = (...p) => el('strong', ...p);
+export const mark       = (...p) => el('mark', ...p);
 
-export const input = (...rest) =>
-  reactComponent('input', { type: 'text' }, ...rest);
-export const button = (...rest) => reactComponent('button', ...rest);
-export const text = input;
-export const textarea = (...rest) => reactComponent('textarea', ...rest);
-export const checkbox = (...rest) =>
-  reactComponent('input', { type: 'checkbox' }, ...rest);
-export const radio = (...rest) =>
-  reactComponent('input', { type: 'radio' }, ...rest);
-export const select = (...rest) => reactComponent('select', ...rest);
-export const option = (...rest) => reactComponent('option', ...rest);
-export const file = (...rest) =>
-  reactComponent('input', { type: 'file' }, ...rest);
+export const input      = (...p) => el('input', { type: 'text' }, ...p);
+export const button     = (...p) => el('button', ...p);
+export const text       = input;
+export const textarea   = (...p) => el('textarea', ...p);
+export const checkbox   = (...p) => el('input', { type: 'checkbox' }, ...p);
+export const radio      = (...p) => el('input', { type: 'radio' }, ...p);
+export const select     = (...p) => el('select', ...p);
+export const option     = (...p) => el('option', ...p);
+export const file       = (...p) => el('input', { type: 'file' }, ...p);
 
-export const br = (...rest) => reactComponent('br', ...rest);
-export const hr = (...rest) => reactComponent('hr', ...rest);
+export const br         = (...p) => el('br', ...p);
+export const hr         = (...p) => el('hr', ...p);
