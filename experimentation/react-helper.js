3a3def4e00ab7a08694719b5ad9d432f5c8741fa
Justin Makeig <jmpublic+github@makeig.com>
2019-04-09 13:56:48 -0700

    Better currying

--- experimentation/react-helper.js
+++ experimentation/react-helper.js
@@ -53,7 +53,7 @@ function isReact(obj) {
  *                        Iterables will be flattened  and applied recusively
  * @return {React.DetailedReactHTMLElement} Same as `React.createElement`
  */
-function el(name, ...stuff) {
+function componentInstance(name, ...stuff) {
   const props = {};
   const children = [];
 
@@ -90,51 +90,59 @@ export function renderInto(component, element) {
   return ReactDOM.render(component, element);
 }
 
-/* prettier-ignore */ export const toFragment = (...p) => el(React.Fragment, ...p);
+/**
+ * @function
+ * @param {string|React.Component} name - The name or component type
+ * @return {function}
+ */
+const curryComponentInstance = name => (...params) =>
+  componentInstance(name, ...params);
+
+/* prettier-ignore */ export const toFragment = (...p) => componentInstance(React.Fragment, ...p);
 /* prettier-ignore */ export const empty      = ()     => toFragment();
 
-/* prettier-ignore */ export const header     = (...p) => el('header', ...p);
-/* prettier-ignore */ export const nav        = (...p) => el('nav', ...p);
-/* prettier-ignore */ export const footer     = (...p) => el('footer', ...p);
-/* prettier-ignore */ export const div        = (...p) => el('div', ...p);
-/* prettier-ignore */ export const p          = (...p) => el('p', ...p);
-/* prettier-ignore */ export const h1         = (...p) => el('h1', ...p);
-/* prettier-ignore */ export const h2         = (...p) => el('h2', ...p);
-/* prettier-ignore */ export const h3         = (...p) => el('h3', ...p);
-/* prettier-ignore */ export const h4         = (...p) => el('h4', ...p);
-/* prettier-ignore */ export const h5         = (...p) => el('h5', ...p);
-/* prettier-ignore */ export const h6         = (...p) => el('h6', ...p);
+/* prettier-ignore */ export const header     = curryComponentInstance('header');
+/* prettier-ignore */ export const nav        = curryComponentInstance('nav');
+/* prettier-ignore */ export const footer     = curryComponentInstance('footer');
+/* prettier-ignore */ export const div        = curryComponentInstance('div');
+/* prettier-ignore */ export const p          = curryComponentInstance('p');
+/* prettier-ignore */ export const h1         = curryComponentInstance('h1');
+/* prettier-ignore */ export const h2         = curryComponentInstance('h2');
+/* prettier-ignore */ export const h3         = curryComponentInstance('h3');
+/* prettier-ignore */ export const h4         = curryComponentInstance('h4');
+/* prettier-ignore */ export const h5         = curryComponentInstance('h5');
+/* prettier-ignore */ export const h6         = curryComponentInstance('h6');
 
-/* prettier-ignore */ export const ul         = (...p) => el('ul', ...p);
-/* prettier-ignore */ export const ol         = (...p) => el('ol', ...p);
-/* prettier-ignore */ export const li         = (...p) => el('li', ...p);
-/* prettier-ignore */ export const dl         = (...p) => el('dl', ...p);
-/* prettier-ignore */ export const dt         = (...p) => el('dt', ...p);
-/* prettier-ignore */ export const dd         = (...p) => el('dd', ...p);
+/* prettier-ignore */ export const ul         = curryComponentInstance('ul');
+/* prettier-ignore */ export const ol         = curryComponentInstance('ol');
+/* prettier-ignore */ export const li         = curryComponentInstance('li');
+/* prettier-ignore */ export const dl         = curryComponentInstance('dl');
+/* prettier-ignore */ export const dt         = curryComponentInstance('dt');
+/* prettier-ignore */ export const dd         = curryComponentInstance('dd');
 
-/* prettier-ignore */ export const table      = (...p) => el('table', ...p);
-/* prettier-ignore */ export const thead      = (...p) => el('thead', ...p);
-/* prettier-ignore */ export const tfoot      = (...p) => el('tfoot', ...p);
-/* prettier-ignore */ export const tbody      = (...p) => el('tbody', ...p);
-/* prettier-ignore */ export const tr         = (...p) => el('tr', ...p);
-/* prettier-ignore */ export const th         = (...p) => el('th', ...p);
-/* prettier-ignore */ export const td         = (...p) => el('td', ...p);
+/* prettier-ignore */ export const table      = curryComponentInstance('table');
+/* prettier-ignore */ export const thead      = curryComponentInstance('thead');
+/* prettier-ignore */ export const tfoot      = curryComponentInstance('tfoot');
+/* prettier-ignore */ export const tbody      = curryComponentInstance('tbody');
+/* prettier-ignore */ export const tr         = curryComponentInstance('tr');
+/* prettier-ignore */ export const th         = curryComponentInstance('th');
+/* prettier-ignore */ export const td         = curryComponentInstance('td');
 
-/* prettier-ignore */ export const span       = (...p) => el('span', ...p);
-/* prettier-ignore */ export const a          = (...p) => el('a', ...p);
-/* prettier-ignore */ export const em         = (...p) => el('em', ...p);
-/* prettier-ignore */ export const strong     = (...p) => el('strong', ...p);
-/* prettier-ignore */ export const mark       = (...p) => el('mark', ...p);
+/* prettier-ignore */ export const span       = curryComponentInstance('span');
+/* prettier-ignore */ export const a          = curryComponentInstance('a');
+/* prettier-ignore */ export const em         = curryComponentInstance('em');
+/* prettier-ignore */ export const strong     = curryComponentInstance('strong');
+/* prettier-ignore */ export const mark       = curryComponentInstance('mark');
 
-/* prettier-ignore */ export const input      = (...p) => el('input', { type: 'text' }, ...p);
-/* prettier-ignore */ export const button     = (...p) => el('button', ...p);
+/* prettier-ignore */ export const input      = (...p) => componentInstance('input', { type: 'text' }, ...p);
+/* prettier-ignore */ export const button     = curryComponentInstance('button');
 /* prettier-ignore */ export const text       = input;
-/* prettier-ignore */ export const textarea   = (...p) => el('textarea', ...p);
-/* prettier-ignore */ export const checkbox   = (...p) => el('input', { type: 'checkbox' }, ...p);
-/* prettier-ignore */ export const radio      = (...p) => el('input', { type: 'radio' }, ...p);
-/* prettier-ignore */ export const select     = (...p) => el('select', ...p);
-/* prettier-ignore */ export const option     = (...p) => el('option', ...p);
-/* prettier-ignore */ export const file       = (...p) => el('input', { type: 'file' }, ...p);
+/* prettier-ignore */ export const textarea   = curryComponentInstance('textarea');
+/* prettier-ignore */ export const checkbox   = (...p) => componentInstance('input', { type: 'checkbox' }, ...p);
+/* prettier-ignore */ export const radio      = (...p) => componentInstance('input', { type: 'radio' }, ...p);
+/* prettier-ignore */ export const select     = curryComponentInstance('select');
+/* prettier-ignore */ export const option     = curryComponentInstance('option');
+/* prettier-ignore */ export const file       = (...p) => componentInstance('input', { type: 'file' }, ...p);
 
-/* prettier-ignore */ export const br         = (...p) => el('br', ...p);
-/* prettier-ignore */ export const hr         = (...p) => el('hr', ...p);
+/* prettier-ignore */ export const br         = curryComponentInstance('br');
+/* prettier-ignore */ export const hr         = curryComponentInstance('hr');
